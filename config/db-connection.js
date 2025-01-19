13a60813ea7c90f63aa375ccdfab874ec6523db6
Afshal Hassan <102484020+Afshal-Hassan@users.noreply.github.com>
2025-01-19 14:02:36 +0500

    feat: google auth

---
+++ config/db-connection.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGODB_URL_AND_DATABASE)
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((error) => {
+    console.log("No database connection");
+    console.log(process.env.MONGODB_URL_AND_DATABASE);
+  });
--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
-require("dotenv").config;
 const cors = require("cors");
 const error = require("./middlewares/error-handler");
 const wordRoute = require("./routes/wordRoute");
+const authRoute = require("./routes/auth-route");
+
+//  ************        Environment variables configuration     **************
+
+require("dotenv").config();
+
+//  **********      Database Connection         **********
+require("./config/db-connection");
 
 const app = express();
 
@@ -10,6 +17,11 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/sign", wordRoute);
+app.use("/api/auth", authRoute);
+
+app.get("/test", async (req, res) => {
+  res.send("Server is running");
+});
 
 const PORT = process.env.PORT || 3000;
 
